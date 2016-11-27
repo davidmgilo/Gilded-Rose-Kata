a288a2ade96a5690be79c9a8be4cf70185458554
David Martinez Gilo <davidmgilo@gmail.com>
2016-11-28 00:01:04 +0100

    added tests for normal items from oldtests

--- spec/GildedRoseSpec.php
+++ spec/GildedRoseSpec.php
@@ -22,4 +22,29 @@ class GildedRoseSpec extends ObjectBehavior
         $this->quality->shouldBe(9);
         $this->sellIn->shouldBe(4);
     }
+
+    //limit case
+    function it_updates_normal_items_on_sell_date()
+    {
+        $this->beConstructedWith('normal',10,0);
+        $this->tick();
+        $this->quality->shouldBe(8);
+        $this->sellIn->shouldBe(-1);
+    }
+
+    function it_updates_normal_items_after_sell_date()
+    {
+        $this->beConstructedWith('normal',10,-5);
+        $this->tick();
+        $this->quality->shouldBe(8);
+        $this->sellIn->shouldBe(-6);
+    }
+
+    function it_updates_normal_items_with_a_quality_of_0()
+    {
+        $this->beConstructedWith('normal',0,5);
+        $this->tick();
+        $this->quality->shouldBe(0);
+        $this->sellIn->shouldBe(4);
+    }
 }
