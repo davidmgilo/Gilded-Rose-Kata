82a535b064f1c36427bcb1a70c17673b824ccfcd
David Martinez Gilo <davidmgilo@gmail.com>
2016-11-27 22:28:06 +0100

    added test1 from acacha/Gilded-Rose

--- spec/GildedRoseSpec.php
+++ spec/GildedRoseSpec.php
@@ -14,4 +14,12 @@ class GildedRoseSpec extends ObjectBehavior
         $this->beConstructedWith('normal',10,5);
         $this->shouldHaveType(GildedRose::class);
     }
+
+    function it_updates_normal_items_before_sell_date()
+    {
+        $this->beConstructedWith('normal',10,5);
+        $this->tick();
+        $this->quality->shouldBe(9);
+        $this->sellIn->shouldBe(4);
+    }
 }
