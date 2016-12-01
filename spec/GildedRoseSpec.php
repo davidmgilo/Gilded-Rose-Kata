3b64fde9e0f9c48acd5c89b2f20fed27a06d735b
David Martinez <davidmgilo@gmail.com>
2016-12-01 16:12:03 +0100

    AgedBrie cases & class ok

--- spec/GildedRoseSpec.php
+++ spec/GildedRoseSpec.php
@@ -79,61 +79,61 @@ class GildedRoseSpec extends ObjectBehavior
         $this->sellIn->shouldBe(4);
     }
 
-//    function it_updates_Brie_items_before_the_sell_date()
-//    {
-//        $this->beConstructedWith('Aged Brie',10,5);
-//        $this->tick();
-//        $this->quality->shouldBe(11);
-//        $this->sellIn->shouldBe(4);
-//    }
-//
-//    function it_updates_Brie_items_before_the_sell_date_with_maximum_quality()
-//    {
-//        $this->beConstructedWith('Aged Brie',50,5);
-//        $this->tick();
-//        $this->quality->shouldBe(50);
-//        $this->sellIn->shouldBe(4);
-//    }
-//
-//    function it_updates_Brie_items_on_the_sell_date()
-//    {
-//        $this->beConstructedWith('Aged Brie',10,0);
-//        $this->tick();
-//        $this->quality->shouldBe(12);
-//        $this->sellIn->shouldBe(-1);
-//    }
-//
-//    function it_updates_Brie_items_on_the_sell_date_near_maximum_quality()
-//    {
-//        $this->beConstructedWith('Aged Brie',49,0);
-//        $this->tick();
-//        $this->quality->shouldBe(50);
-//        $this->sellIn->shouldBe(-1);
-//    }
-//
-//    function it_updates_Brie_items_on_the_sell_date_with_maximum_quality()
-//    {
-//        $this->beConstructedWith('Aged Brie',50,0);
-//        $this->tick();
-//        $this->quality->shouldBe(50);
-//        $this->sellIn->shouldBe(-1);
-//    }
-//
-//    function it_updates_Brie_items_after_the_sell_date()
-//    {
-//        $this->beConstructedWith('Aged Brie',10,-10);
-//        $this->tick();
-//        $this->quality->shouldBe(12);
-//        $this->sellIn->shouldBe(-11);
-//    }
-//
-//    function it_updates_Brie_items_after_the_sell_date_with_maximum_quality()
-//    {
-//        $this->beConstructedWith('Aged Brie',50,-10);
-//        $this->tick();
-//        $this->quality->shouldBe(50);
-//        $this->sellIn->shouldBe(-11);
-//    }
+    function it_updates_Brie_items_before_the_sell_date()
+    {
+        $this->beConstructedThrough('of',['Aged Brie',10,5]);
+        $this->tick();
+        $this->quality->shouldBe(11);
+        $this->sellIn->shouldBe(4);
+    }
+
+    function it_updates_Brie_items_before_the_sell_date_with_maximum_quality()
+    {
+        $this->beConstructedThrough('of',['Aged Brie',50,5]);
+        $this->tick();
+        $this->quality->shouldBe(50);
+        $this->sellIn->shouldBe(4);
+    }
+
+    function it_updates_Brie_items_on_the_sell_date()
+    {
+        $this->beConstructedThrough('of',['Aged Brie',10,0]);
+        $this->tick();
+        $this->quality->shouldBe(12);
+        $this->sellIn->shouldBe(-1);
+    }
+
+    function it_updates_Brie_items_on_the_sell_date_near_maximum_quality()
+    {
+        $this->beConstructedThrough('of',['Aged Brie',49,0]);
+        $this->tick();
+        $this->quality->shouldBe(50);
+        $this->sellIn->shouldBe(-1);
+    }
+
+    function it_updates_Brie_items_on_the_sell_date_with_maximum_quality()
+    {
+        $this->beConstructedThrough('of',['Aged Brie',50,0]);
+        $this->tick();
+        $this->quality->shouldBe(50);
+        $this->sellIn->shouldBe(-1);
+    }
+
+    function it_updates_Brie_items_after_the_sell_date()
+    {
+        $this->beConstructedThrough('of',['Aged Brie',10,-10]);
+        $this->tick();
+        $this->quality->shouldBe(12);
+        $this->sellIn->shouldBe(-11);
+    }
+
+    function it_updates_Brie_items_after_the_sell_date_with_maximum_quality()
+    {
+        $this->beConstructedThrough('of',['Aged Brie',50,-10]);
+        $this->tick();
+        $this->quality->shouldBe(50);
+        $this->sellIn->shouldBe(-11);
+    }
 //
 //    function it_updates_Sulfuras_items_before_the_sell_date()
 //    {
